9e5b88f825b6cc6642da6cc91f70b03cbe42ff70
Sakhile Mpungose <sakhi.mpungose365@gmail.com>
2020-08-03 15:37:59 +0200

    Making fixes

--- django_reporting_project/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'dashboard'
-urlpatterns = [
-    path('report/retail', views.ecommerce_report_page, name='retail_report'),
-    path('report/marketing', views.marketing_report_page, name='marketing_report'),
-    path('report/task', views.task, name='task'),
-]
